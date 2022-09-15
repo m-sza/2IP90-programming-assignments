3190d51e32215b1010b6f334188561a0427ae429
Szabolcs <matyi.szabolcs@gmail.com>
2022-09-15 10:31:33 +0200

    human

---
+++ human_vs_computer/HumanGame.java
@@ -0,0 +1,29 @@
+import java.util.*; // For Scanner, Random, etc.
+
+/** 
+ * Number guessing game for humans.
+ * 
+ * Enter a seed number, and the computer will think of a number between 0 and
+ * 99 that you have to guess in at most seven tries. Afterwards, you see your
+ * guessing history so you can learn to better play the game.
+ * 
+ * @author TODO
+ * @id     TODO
+ * @author TODO
+ * @id     TODO
+ * @data   TODO
+ */
+public class HumanGame {
+    Scanner sc = new Scanner(System.in);
+    Random randomGenerator;
+
+    void run() {
+        // TODO: Implementation
+      
+        // END TODO
+    }
+
+    public static void main(String[] args) {
+        new HumanGame().run();
+    }
+}
