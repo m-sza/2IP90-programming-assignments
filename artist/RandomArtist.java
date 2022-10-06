2791ba08607e6e5bc0ac1bf217902273f52fe4ba
Szabolcs <matyi.szabolcs@gmail.com>
2022-10-06 11:35:07 +0200

    umpload artist

---
+++ artist/RandomArtist.java
@@ -0,0 +1,54 @@
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/** 
+ * Main class for the homework assignment Random Artist.
+ * 
+ * This class doesn't need to change.
+ *  
+ * TODO:
+ *
+ * @author NAME
+ * @id ID
+ * @author NAME
+ * @id ID
+ */
+public class RandomArtist {
+    JFrame frame;
+    Painting painting; // panel that provides the random painting
+    JButton regenerateButton;
+    JButton shotButton;
+
+    /**
+     * Create a new instance of the RandomArtist application.
+     */
+    RandomArtist() {
+        // invokeLater: preferred way to create components
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                painting = new Painting();
+                frame = new JFrame("Computer Assisted Random Artist");
+                frame.add(painting, BorderLayout.CENTER);
+                regenerateButton = new JButton("Regenerate");
+
+                // painting provides reaction to buttonclick
+                regenerateButton.addActionListener(painting); 
+
+                frame.add(regenerateButton, BorderLayout.SOUTH);
+                shotButton = new JButton("Screenshot");
+                shotButton.addActionListener(painting);
+                frame.add(shotButton, BorderLayout.NORTH);
+                frame.pack();
+                painting.regenerate(); // can be done here since painting has a size!
+                frame.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] arg) {
+        new RandomArtist();
+    }
+}
